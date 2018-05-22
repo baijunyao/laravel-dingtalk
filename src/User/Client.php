4184df4756361c45b75b005efe9d172d6d02f0e6
baijunyao <baijunyao@baijunyao.com>
2018-05-22 15:02:56 +0800

    获取全部员工的all方法完成

--- src/User/Client.php
+++ src/User/Client.php
@@ -20,6 +20,24 @@ use EasyDingTalk\User\Client as BaseUserClient;
  */
 class Client extends BaseUserClient
 {
+    /**
+     * 获取全部员工
+     *
+     * @return array
+     */
+    public function all()
+    {
+        // 获取全部部门
+        $department = $this->app->department->list();
+        $userList = collect([]);
+        foreach ($department['department'] as $k => $v) {
+            // 获取部门下的成员
+            $user = $this->app->user->list($v['id']);
+            $userList = $userList->merge($user['userlist']);
+        }
+        // 因为员工可能存在跨部门的情况；所以此处需要根据员工 unionid 去重
+        return $userList->unique('unionid');
+    }
 
     /**
      * @param int   $departmentId
